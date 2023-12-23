4222bf747e54dde263d3ce3590299f7df85d8070
ANUPAM-RO <anupamroy575@gmail.com>
2023-12-23 20:10:54 +0530

    list page added

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -11,6 +11,8 @@ import { Button } from "../ui/button"
 import { X } from "lucide-react"
 import { toast } from "sonner"
 import { FormPicker } from "./form-picker"
+import { ElementRef, useRef } from "react"
+import { useRouter } from "next/navigation"
 
 interface FormPopoverProps{
     children: React.ReactNode;
@@ -24,14 +26,16 @@ export const FormPopover = ({
     align,
     sideOffset = 0
 }: FormPopoverProps) => {
+    const router = useRouter()
+    const closeRef = useRef<ElementRef<"button">>(null)
 
     const { execute, fieldErrors } = useAction(createBoard, {
         onSuccess: (data) => {
-            console.log({ data });
-            toast.success("Board created!")
+            toast.success("Board created!");
+            closeRef.current?.click();
+            router.push(`/board/${data.id}`)
         },
         onError: (error) => {
-            console.log({ error })
             toast.error(error)
         }
     });
@@ -39,7 +43,6 @@ export const FormPopover = ({
     const onSubmit = (formData: FormData) => {
         const title = formData.get("title") as string;
         const image = formData.get("image") as string;
-        console.log(image)
         execute({image , title})
     }
     return (
@@ -49,7 +52,7 @@ export const FormPopover = ({
             </PopoverTrigger>
             <PopoverContent align={align} className="w-80 pt-3" side={side} sideOffset={sideOffset}>
                 <div className="text-sm font-medium text-center text-neutral-600 pb-4">Create board</div>
-                <PopoverClose asChild>
+                <PopoverClose ref={closeRef} asChild>
                     <Button className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600" variant="ghost">
                         <X className="h-4 w-4 border-none"/>
                     </Button>
